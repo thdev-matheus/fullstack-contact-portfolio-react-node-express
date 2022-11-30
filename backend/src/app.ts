94e839ac4e48d6aea6bdebaa64f82a188a353e60
Theus <matheusth.dev@gmail.com>
2022-11-30 15:38:37 -0300

    feat: dockerization of database and backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,21 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
 import "express-async-errors";
 import { handleErrorMiddleware } from "./errors";
 import { appRoutes } from "./routes";
 
 export const app = express();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-PINGOTHER, Content-Type, Authorization"
+  );
+  app.use(cors());
+  next();
+});
 
 app.use(express.json());
 
