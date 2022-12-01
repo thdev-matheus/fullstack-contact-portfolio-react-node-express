c2f774f2f1085a2f212da8ce7b9284d80a4049ad
Theus <matheusth.dev@gmail.com>
2022-12-01 13:04:31 -0300

    feat: contact type and schema

--- frontend/src/globalTypes/index.ts
+++ frontend/src/globalTypes/index.ts
@@ -1,15 +1,3 @@
-export interface IFormData {
-  name?: string | undefined;
-  fullName?: string | undefined;
-  email?: string | undefined;
-  email1?: string | undefined;
-  email2?: string | undefined;
-  phone1?: string | undefined;
-  phone2?: string | undefined;
-  password?: string | undefined;
-  confirmPassword?: string | undefined;
-}
-
 export interface IRegisterData {
   fullName: string;
   email1: string;
@@ -23,4 +11,12 @@ export interface IRegisterData {
 export interface ILoginData {
   email: string;
   password: string;
+}
+
+export interface IContactData {
+  name: string;
+  email1: string;
+  email2?: string;
+  phone1: string;
+  phone2?: string;
 }
--- frontend/src/schemas/index.ts
+++ frontend/src/schemas/index.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 import { SchemaOf } from "yup";
-import { ILoginData, IRegisterData } from "../globalTypes";
+import { IContactData, ILoginData, IRegisterData } from "../globalTypes";
 
 export const registerSchema: SchemaOf<IRegisterData> = yup.object().shape({
   fullName: yup.string().required("Campo obrigatório!").min(5),
@@ -40,4 +40,24 @@ export const loginSchema: SchemaOf<ILoginData> = yup.object().shape({
   email: yup.string().email("Email inválido").required("Campo obrigatório!"),
 
   password: yup.string().required("Campo obrigatório!"),
+});
+
+export const contactSchema: SchemaOf<IContactData> = yup.object().shape({
+  name: yup.string().required("Campo obrigatório!").min(5),
+
+  email1: yup.string().email("Email inválido").required("Campo obrigatório!"),
+
+  email2: yup.string().email("Email inválido"),
+
+  phone1: yup
+    .string()
+    .required("Campo obrigatório!")
+    .min(10, "Número inválido")
+    .max(11, "Número inválido")
+    .matches(
+      /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+      "Número inválido"
+    ),
+
+  phone2: yup.string(),
 });
